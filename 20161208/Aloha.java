33d97af1f3d56d67d03152eb3d2058e018543361
り　けつ <yse160136@gmail.com>
2016-12-12 13:02:38 +0900

    1208

---
+++ 20161208/Aloha.java
@@ -0,0 +1,15 @@
+public class Aloha{
+	public static void main(String[] args) {
+		
+		System.out.println("名前を入力してください");
+		System.out.println(args[0]);
+		String data=new java.util.Scanner (System.in).nextLine;
+		System.out.println(data);
+
+		if (data.equals("aoki")) {
+			break;
+		}else{
+		System.out.println("名前："+args[0]);
+		}
+	}
+}
