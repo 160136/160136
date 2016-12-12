b28a12ef00252f68711b62823139292f81f58e13
り　けつ <yse160136@gmail.com>
2016-12-12 13:19:49 +0900

    1212

---
+++ 20161212/Scanner.java
@@ -0,0 +1,11 @@
+public class Scanner{
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		int i=0;
+		while(i<20){
+			i++;
+			System.out.println("i"+args[1]);
+		}
+	}
+	
+}
