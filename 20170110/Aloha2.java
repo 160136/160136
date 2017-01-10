37b8410d29ee3d2ef3a642736e451e2c8a1e6259
り　けつ <yse160136@gmail.com>
2017-01-10 12:03:15 +0900

    Aloha1 done.

---
+++ 20170110/Aloha2.java
@@ -0,0 +1,41 @@
+  public class Aloha2{
+	public static void main(String[] args) {
+		Alohaman am = new Alohaman(args[0],args[1]);
+		int data1=Integer.parseInt(args[2]);
+		int data2=Integer.parseInt(args[3]);
+		am.sayAloha(data1,data2);
+	}
+}
+class Alohaman{
+	private String msg;
+	private String msg2;
+	
+
+/*	public Alohaman(){
+		this.msg = "ALOHA!";
+		this.msg2 = "アロハ！";
+	} */
+
+	public Alohaman(String m1,String m2){
+		this.msg = m1;
+		this.msg2 = m2;
+	}
+
+	public void sayAloha(){
+		System.out.println(this.msg2);
+	}
+	public void sayAloha(int n){
+		for(int i=0;i<n; i++){
+			System.out.println(i+  "  " + this.msg);
+		}
+	}
+	public void sayAloha(int n,int x){
+		for(int i=0;i<n; i++){
+			if(i%x==0){
+				System.out.println(i+  "  " + this.msg2);
+			}else{
+		System.out.println(i+  "  " + this.msg);
+			}
+		}
+	}
+}
